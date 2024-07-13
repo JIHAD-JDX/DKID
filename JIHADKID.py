c96ef291669a2282e895ddcf2aec4d0bce3dfe73
JIHAD-JDX <miuwhdhshsiridhh@gmail.com>
2024-07-13 18:22:17 +0200

    Add files via upload

---
+++ JIHADKID.py
@@ -0,0 +1,16 @@
+import os 
+import JIHADKID
+import struct
+
+def get_architecture():
+  """Returns '32bit' or '64bit' based on system architecture."""
+  return '64bit' if struct.calcsize('P') * 8 == 64 else '32bit'
+
+
+architecture = get_architecture()
+
+
+if architecture == "32bit":
+    import p32 
+elif architecture == "64bit":
+    import p64
